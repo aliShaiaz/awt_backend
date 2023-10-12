1d305d45f1548d38ad0ad6f19e928cadbc8b7f35
MohammadSheakh <mohammad.sheakh@gmail.com>
2023-10-12 12:40:06 +0600

    running

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+const config:any | PostgresConnectionOptions = {
+  type: 'postgres', 
+  host:'localhost', // database host name 
+  post: 5432, 
+  username: 'postgres', // new
+  password : '', //🔰 give password
+  //password:process.env.DB_PASSWORD,
+  database : 'awt_backend', // database name  
+  //entities : [Feedback, User], // dist er pore src thakbe na 
+  autoLoadEntities : true, // // not for production version 
+  synchronize : true, // production e true rakha jabe na  // 
+  
+}
+
+export default config;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,55 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../ormconfig';
+import { RouterModule } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    // Modules should be register here
+    //.
+    //.
+    //.
+    RouterModule.register([
+      // 4 object should be register here 
+      
+    ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ],
+  providers: [],
+  
 })
 export class AppModule {}
+
+/*
+it should be like this ...............
+@Module({
+    imports: [
+    TypeOrmModule.forRoot(config),
+    SellerModule, 
+        BuyerModule,
+        RouterModule.register([
+      {
+        path:'api/seller', 
+        module : SellerModule
+      },
+      {
+        path:'api/buyer',
+        module : BuyerModule
+      }
+    ]),
+    ConfigModule.forRoot({ //for env variable
+      isGlobal:true
+    }),
+    
+  ],
+  providers: [],
+  
+})
+*/
