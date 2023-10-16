4bd6daeb6774bca657896da0f0a9306c53ce639b
MohammadSheakh <mohammad.sheakh@gmail.com>
2023-10-16 12:49:40 +0600

    working on product sorting

--- src/product-sorting/product-sorting.service.ts
+++ src/product-sorting/product-sorting.service.ts
@@ -1,20 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/seller/entities/product/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductSortingService {
-  
+  constructor(
+    @InjectRepository(Product) private productRepository: Repositoryy<Product>,
+   ){}
+  async sortByMinPriceRange(min: number) {
+    // find those product whose price is minium to min number and higher
+    return await this.productRepository
+      .createQueryBuilder('product')
+      .where('product.price >= :min', { min })
+      .getMany();
 
-  findAll() {
+    
+  }
+
+  async sortAllByMaxPriceRange(max : number) {
+    // return `maximum price range `+ max;
+    return await this.productRepository
+      .createQueryBuilder('product')
+      .where('product.price <= :max', { max })
+      .getMany();
+  }
+
+  async sortAllByMinAndMaxPriceRange(min, max) {
+    
+
+    if(min && max){
+      return await this.productRepository
+      .createQueryBuilder('product')
+      .where('product.price >= :min', { min })
+      .andWhere('product.price <= :max', { max })
+      .getMany();
+    }
+
+    
+  }
+
+  sortProductByBrand() {
     return `This action returns all productSorting`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} productSorting`;
-  }
-
-  
-
-  remove(id: number) {
-    return `This action removes a #${id} productSorting`;
+  sortProductByCategory() {
+    return `This action returns all productSorting`;
   }
 }
